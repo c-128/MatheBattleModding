4daf3aee875a433610cf247ed7061ff45097c56e
c-128 <c-128>
2023-03-06 19:45:54 +0100

    Fix userscript

--- dist/script.user.js
+++ dist/script.user.js
@@ -6,6 +6,6 @@
 // @match        https://mathebattle.de/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=mathebattle.de
 // @grant        none
-// @require      
+// @require      https://github.com/c-128/MatheBattleModding/raw/master/dist/index.js
 // @run-at       document-start
 // ==/UserScript==
